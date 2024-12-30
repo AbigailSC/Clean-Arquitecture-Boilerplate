56b9d3d4ab088aa232a2a97176a2ce18fb7953fb
Abigailsc <abigailsarzuri@gmail.com>
2024-12-30 15:01:24 -0300

    feat: :zap: Added postgres db

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,5 +5,11 @@ export const envs = {
   PORT: get('PORT').required().asPortNumber(),
   MONGO_URL: get('MONGO_URL').required().asString(),
   MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
+  MONGO_USER: get('MONGO_USER').required().asString(),
+  MONGO_PASS: get('MONGO_PASS').required().asString(),
   JWT_SEED: get('JWT_SEED').required().asString(),
+  POSTGRES_USER: get('POSTGRES_USER').required().asString(),
+  POSTGRES_PASS: get('POSTGRES_PASS').required().asString(),
+  POSTGRES_DB: get('POSTGRES_DB').required().asString(),
+  POSTGRES_URL: get('POSTGRES_URL').required().asString(),
 };
